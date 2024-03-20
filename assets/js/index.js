db5e5b0781993b0fb9c4a6ff196116f87a53f8c2
WassCodeur <wachioubouraima56@gmail.com>
2024-03-20 19:31:52 +0000

    Update color styles in index.js and update links in index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 const mode_button = document.getElementById('mode-button');
 const logo = document.getElementById('logo')
 const body = document.body;
+const intro = document.querySelectorAll('.intro');
+const about = document.querySelector('.about-text');
 const menu = document.querySelector('.menu ul li a');
 const link = document.querySelectorAll('a');
 const icon = document.querySelector('.bi-brightness-high');
@@ -18,14 +20,17 @@ function modeToggle() {
     mode_button.innerHTML = "<i class='bi bi-brightness-high' style='color: white'></i>";
     logo.innerHTML = `<img src="/assets/images/whitelogo.png" alt="logo" width="200px">`
     body.style.color = "white"
+    about.style.color = "white"
     changeColor(link, "white")
+    changeColor(intro, "white")
   } else {
     body.style.backgroundColor = "whitesmoke";
     mode_button.innerHTML = "<i class='bi bi-moon'></i>";
     logo.innerHTML = `<img src="/assets/images/blacklogo.png" alt="logo" width="200px">`
     body.style.color = "black"
+    about.style.color = "black"
     changeColor(link, "black")
-
+    changeColor(intro, "black")
   }
 
 
